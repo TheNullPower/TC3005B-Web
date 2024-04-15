277f46ad8092631f5a81e25f35aa8fb80e570005
TheNullPower <oscaritoariel_23of@hotmail.com>
2024-04-14 22:42:35 -0600

    Perfil con Tailwind
    Creación de perfil utilizando Tailwind

---
+++ 03-tailwind/src/App.tsx
@@ -0,0 +1,15 @@
+import Links from "./Components/Links";
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Oscar Ariel Ortega Franco</h1>
+      <Links />
+    </>
+  )
+}
+
+export default App
---
+++ 03-tailwind/src/Components/Links.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Links = () => {
+  return (
+    <div>
+      <p>
+        Encuéntrame en :
+        <div>
+          <a href="https://www.linkedin.com/in/oscar-ariel-ortega-franco-5826571b4" target="_blank" rel="noopener noreferrer">
+            <u>LinkedIn</u>
+          </a>
+        </div>
+        <div>
+          <a href="https://www.facebook.com/oscar.ortegafranco.18/" target="_blank" rel="noopener noreferrer">
+            <u>Facebook</u>
+          </a>
+        </div>
+        <div>
+          <a href="https://www.instagram.com/oscar.ortega.23/" target="_blank" rel="noopener noreferrer">
+            <u>Instagram</u>
+          </a>
+        </div>
+      </p>
+    </div>
+  );
+}
+
+export default Links;
